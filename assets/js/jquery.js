dc1b85c528f1ce1ec68b4f5d3dde1c7ac64a769a
unicornelia <ez.kornelia@gmail.com>
2016-12-09 16:02:29 +0000

    Added city and weather api info

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,59 @@
 $(document).ready(function () {
-  $('#temperature-reset').click(function() {
-    alert('Help');
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $("#temperature-up").click(function() { //event listener
+    thermostat.increase(1); //update model
+    updateTemperature(); //update view
   });
+
+  $("#temperature-down").click(function() { //event listener
+    thermostat.decrease(1); //update model
+    updateTemperature(); //update view
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.powerSavingOn();
+    $('#powersaving').text('Power Saving On')
+    updateTemperature(); //update view
+  });
+
+  $('#powersaving-off').click(function() {
+    thermostat.powerSavingOff();
+    $('#powersaving').text('Power Saving Off')
+    updateTemperature(); //update view
+  });
+
+  $("#reset").click(function() { //event listener
+    thermostat.reset(); //update model
+    updateTemperature(); //update view
+  });
+
+  $('#energy-usage').click(function() {
+    thermostat.energyUsage();
+    updateUsage();
+  });
+
+  $('#current-city').change(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.degrees);
+  };
+
+  function updateUsage() {
+    $('#energy-display').attr('class', thermostat.energyUsage());
+  };
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=1f8964bd5eb43e2d0df92ec8d112b4b8';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  };
 });
