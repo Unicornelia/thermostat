5dc44ea21158506f06d1a259341e43d2d6243865
unicornelia <ez.kornelia@gmail.com>
2016-12-22 20:03:04 +0000

    Rearranging files for sinatra

--- app/thermostat_app.rb
+++ app/thermostat_app.rb
@@ -2,8 +2,9 @@ require 'sinatra/base'
 
 class Thermostat < Sinatra::Base
   get '/' do
-    'Hello Thermostat!'
+    erb :index
   end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
