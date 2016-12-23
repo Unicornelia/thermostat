054e0e1626dfb3d2a6f46412691b0860ad5b0190
unicornelia <ez.kornelia@gmail.com>
2016-12-23 11:40:49 +0000

    App works fully, fixed routes on about.erb too

--- app/thermostat_app.rb
+++ app/thermostat_app.rb
@@ -6,10 +6,12 @@ class Thermostat < Sinatra::Base
   use Rack::MethodOverride
 
   get '/' do
-    erb :'index'
+    erb :index
   end
 
-
+  get '/about' do
+    erb :about
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
